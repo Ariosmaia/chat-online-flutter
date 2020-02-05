6a49cbd9ac4cdaafd76dd747cd44ff5da34cc2ff
Allan <ariosmaia@yahoo.com.br>
2020-02-05 16:56:35 -0300

    Realizando o Login com o Google

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -11,8 +13,69 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Sempre que a autenticação mudar
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<FirebaseUser> _getUser() async {
+    if (_currentUser != null) return _currentUser;
+
+    try {
+      // Login no Google
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+
+      // Transformar login do Google em um Login do Firebase
+      // Tem um token e um token de acesso
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      // Credeciais do Firebase
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      // Login no Firebase
+      final AuthResult authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      // Pegar usuario do Firebase
+      final FirebaseUser user = authResult.user;
+
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
   void _sendMessage({String text, File imgFile}) async {
-    Map<String, dynamic> data = {};
+    final FirebaseUser user = await _getUser();
+
+    if (user == null) {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Não foi possível fazer o login. Tente novamente!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user.uid,
+      "sendName": user.displayName,
+      "senderPhotoUrl": user.photoUrl
+    };
 
     if (imgFile != null) {
       StorageUploadTask task = FirebaseStorage.instance
@@ -35,6 +98,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Olá'),
         elevation: 0,
@@ -43,28 +107,29 @@ class _ChatScreenState extends State<ChatScreen> {
         children: <Widget>[
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
-                stream: Firestore.instance.collection('messages').snapshots(),
-                builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                    case ConnectionState.waiting:
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    default:
-                      List<DocumentSnapshot> documents =
-                          snapshot.data.documents.reversed.toList();
-                      return ListView.builder(
-                        itemCount: documents.length,
-                        reverse: true,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(documents[index].data['text']),
-                          );
-                        },
-                      );
-                  }
-                }),
+              stream: Firestore.instance.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data.documents.reversed.toList();
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(documents[index].data['text']),
+                        );
+                      },
+                    );
+                }
+              },
+            ),
           ),
           TextComposer(sendMessage: _sendMessage),
         ],
