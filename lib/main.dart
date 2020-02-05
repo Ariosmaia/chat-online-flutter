a7f0e41380bd57d29683771fea8ef8db9c063e82
Allan <ariosmaia@yahoo.com.br>
2020-02-05 13:35:31 -0300

    4. Criando o campo de digitação

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
 
 void main() async {
   runApp(MyApp());
-
-  // Ler somente uma vez
-  QuerySnapshot snapshot =
-      await Firestore.instance.collection('mensagens').getDocuments();
-  snapshot.documents.forEach((d) {
-    print(d.data);
-
-    // Ler pelo ID do documento
-    print(d.documentID);
-
-    // Atualizar cada um dos documentos
-    d.reference.updateData({'read': true});
-  });
-
-  // Dados de uma imagem em especifico
-  DocumentSnapshot snapshotDoc = await Firestore.instance
-      .collection('mensagens')
-      .document('XYyfrLKPzQcH1YNRT6vi')
-      .get();
-  print(snapshotDoc.data);
-
-  // Atualizações em tempo real coleções
-  Firestore.instance.collection('mensagens').snapshots().listen((dado) {
-    dado.documents.forEach((d) {
-      print(d.data);
-    });
-  });
-
-  // Atualizações em tempo real documentos
-  Firestore.instance
-      .collection('mensagens')
-      .document('XYyfrLKPzQcH1YNRT6vi')
-      .snapshots()
-      .listen((dado) {
-    print(dado.data);
-  });
 }
 
 class MyApp extends StatelessWidget {
@@ -49,9 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(color: Colors.blue)),
+      home: ChatScreen(),
     );
   }
 }
