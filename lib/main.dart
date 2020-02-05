ea2b8266d4a804e92502688de03fbee890ea161a
Allan <ariosmaia@yahoo.com.br>
2020-02-05 12:46:38 -0300

    2. Escrevendo Dados no Banco

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 
+  // Escrever ou ler
   Firestore.instance
-      .collection('col')
-      .document('doc')
-      .setData({"texto": "Allan"});
+      // Qual coleção viu usar
+      .collection('mensagens')
+      // Qual o documento
+      // Deixar vazio cria um id unico
+      .document('ble7RdMCzl7se8Mjagu3')
+      // Qual dados vou colocar
+      .setData({"texto": "Td bem?", "from": "Marcelo", "read": false});
+
+  // Para atualizar só um dado posso usar o updateData ao invés de setData
+  Firestore.instance
+      .collection('mensagens')
+      .document('ble7RdMCzl7se8Mjagu3')
+      .updateData({"read": true});
+
+  // Subcoleções
+  Firestore.instance
+      .collection('mensagens')
+      .document('ble7RdMCzl7se8Mjagu3')
+      .collection('arquivos')
+      .document()
+      .setData({"arqname": 'foto.png'});
 }
 
 class MyApp extends StatelessWidget {
